3579bbf3f860f0675ecd5b7980fc0f13c72bf9b5
k7t3 <k7t3annnn@gmail.com>
2024-07-20 16:40:48 +0900

    コピーライトの適用。

--- src/main/java/com/github/k7t3/tcv/domain/channel/StreamInfo.java
+++ src/main/java/com/github/k7t3/tcv/domain/channel/StreamInfo.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 k7t3
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.k7t3.tcv.domain.channel;
 
 import com.github.twitch4j.helix.domain.Stream;
@@ -16,8 +32,7 @@ public record StreamInfo(
         int viewerCount,
         LocalDateTime startedAt,
         Duration uptime,
-        String language,
-        String thumbnailURL
+        String language
 ) {
 
     public static StreamInfo of(Stream stream) {
@@ -31,8 +46,7 @@ public record StreamInfo(
                 stream.getViewerCount(),
                 dateTime,
                 stream.getUptime(),
-                stream.getLanguage(),
-                stream.getThumbnailUrl()
+                stream.getLanguage()
         );
     }
 
